63511908e34bdd0eb0ec0438ee526f2325367bdc
Guilherme Nakazato <guilherme.fernandes23903@gmail.com>
2021-03-20 22:22:11 -0300

    [NEW] Configurando os plugins axios e vuetify e fazendo a estilização inicial da página Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
-    <div>
-        Olá mundo
-    </div>
+  <v-row no-gutters class="home">
+    <v-col>
+      <v-card class="pa-2" tile elevation="0">
+        <v-card-title class="card-title">
+          Gerencie suas tarefas diárias de um jeito mais legal
+        </v-card-title>
+        <v-card-subtitle class="card-subtitle">
+          Com organização e tempo, acha-se o segredo de fazer tudo e bem feito
+        </v-card-subtitle>
+        <v-card-actions>
+          <v-btn class="ma-2" color="secundary"> Criar conta </v-btn>
+          <v-btn class="ma-2" outlined color="indigo">
+            Já possuo uma conta
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col>
+      <v-card class="pa-2" tile elevation="0">
+        <v-img src="@/assets/images/checklist.svg"></v-img
+      ></v-card>
+    </v-col>
+  </v-row>
 </template>
 <script>
 export default {
-    name: "Home",
-}
+  name: "Home",
+};
 </script>
